b371f9b5b0a2df44665030fb4b891c3f5a620dec
Ryan <rc10house@yahoo.com>
2022-08-29 11:47:24 -0500

    Added check for directory functionality

--- gitupdate.py
+++ gitupdate.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 if (len(sys.argv) == 1):
-    print("Usage: gitupdate.py PATH_TO_DIR_OF_GIT_REPOS")
+    print("Usage: gitupdate.py (PATH TO DIR OF GIT REPOS)")
     exit()
 directory = sys.argv[1]
 files = os.listdir(directory)
@@ -10,12 +10,13 @@ os.chdir(directory)
 
 for f in files:
     if f[0] != '.':
-        os.chdir(f)
-        if os.path.isdir(".git"):
-            cmd = "git pull"
-            os.system(cmd)
-            os.chdir("../")
-        else:
-            os.chdir("../")
+        if os.path.isdir(f):
+            os.chdir(f)
+            if os.path.isdir(".git"):
+                cmd = "git pull"
+                os.system(cmd)
+                os.chdir("../")
+            else:
+                os.chdir("../")
 
 
